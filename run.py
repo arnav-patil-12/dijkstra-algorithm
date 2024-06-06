a30564c6d26ab53b09eae8bfe6c38b508610473c
arnav-patil-12 <arnav.patil@mail.utoronto.ca>
2024-06-05 22:27:23 -0400

    First run.py commit

---
+++ run.py
@@ -0,0 +1,12 @@
+from dijkstra import Graph
+
+def create_graph(size):
+    """
+    (int) -> Graph
+
+    Returns a Graph object of size 'size'
+    """
+    graph = Graph(size)
+    return graph
+
+def
